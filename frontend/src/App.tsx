b5b13e1968b6009efe42bd46d1fda9602b26a690
Harsahilbasra <hbas514@aucklanduni.ac.nz>
2026-07-27 21:14:44 +1200

    Add basic navigation between pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,31 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
-import Leaderboard from './pages/Leaderboard'
-import About from './pages/About'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Map from './pages/Map'
+
+
 import './App.css'
 
 export default function App() {
   return (
     <BrowserRouter>
       <header>
-        <h1>MSA Demo App</h1>
+        <h1>Voyage</h1>
         <nav>
-          <NavLink to="/" end>Leaderboard</NavLink>
-          <NavLink to="/about">About</NavLink>
+          <NavLink to="/Login">Login</NavLink>
+          <NavLink to="/Signup">Signup</NavLink>
+          <NavLink to="/Map">Map</NavLink>
+          <NavLink to="/Landing">Landing</NavLink>
         </nav>
       </header>
 
       <main>
         <Routes>
-          <Route path="/" element={<Leaderboard />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Landing/>} />
+          <Route path="/about" element={<Login/>} />
+          <Route path="/about" element={<Signup/>} />
+          <Route path="/about" element={<Map/>} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <h1>Landing Page</h1>
+}
