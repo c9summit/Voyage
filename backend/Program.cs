38df22fd83068508de9850f3c786e082358114e5
Harsahilbasra <hbas514@aucklanduni.ac.nz>
2026-08-09 22:18:25 +1200

    fix error for backend deploy

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,6 +9,9 @@ using backend.Hubs;
 using Microsoft.EntityFrameworkCore.Sqlite;
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367f5c2a42949a5a712ebb24371ded41e013a63c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df180aa728ca1e124ec59baf379620cd39cc3af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
